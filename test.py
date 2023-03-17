a9644411ce072b30c419f8b91f734783002f6ac8
Tin Myat Win <scm.dott.developer6@gmail.com>
2023-03-17 15:12:56 +0630

    First(push first commit)

---
+++ test.py
@@ -0,0 +1,131 @@
+import os
+import requests
+import validators
+from slack_bolt import App
+from slack_sdk import WebClient
+from flask import Flask, request
+from bs4 import BeautifulSoup
+from slack_bolt.adapter.flask import SlackRequestHandler
+
+SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
+SIGNING_SECRET = os.environ.get('SIGNING_SECRET')
+CHANNEL_ID = os.environ.get('CHANNEL_ID')
+SERVER_KEYWORD = os.environ.get('SERVER_KEYWORD')
+MAILBOX_KEYWORD = os.environ.get('MAILBOX_KEYWORD')
+MENTION_USER_WITH_MSG = os.environ.get('MENTION_USER_WITH_MSG')
+
+flask_app = Flask(__name__)
+app = App(
+   signing_secret=os.environ.get('SIGNING_SECRET'),
+   token=os.environ.get('SLACK_BOT_TOKEN'),
+)
+req_handler = SlackRequestHandler(app)
+client = WebClient(token=SLACK_BOT_TOKEN)
+
+
+# Register routes to Flask app
+@flask_app.route('/slack/events', methods=['POST'])
+def slack_events():
+    return req_handler.handle(request)
+
+
+@app.event('message')
+def handle_message_events(body, logger):
+    event = body.get('event', {})
+    print('------- start ------')
+    if 'text' in event:
+        text_link = event.get('text')
+        text_link = text_link[1:]
+        text_link = text_link[:-1]
+        content_res = ''
+        filter_keyword = ''
+        if validators.url(text_link):
+            html = requests.get(text_link)
+            soup = BeautifulSoup(html.text, 'html.parser')
+            entry_data = soup.find_all('div', {'class': 'entry'})
+            for entry in entry_data:
+                content_res = entry.find('div', {'class': 'entry-body'}).text
+
+            if SERVER_KEYWORD in content_res:
+                filter_keyword = '「{0}」'.format(SERVER_KEYWORD)
+            if MAILBOX_KEYWORD in content_res:
+                filter_keyword = '{0}「{1}」'.format(filter_keyword, MAILBOX_KEYWORD)
+
+            if filter_keyword:
+                res = client.chat_postMessage(
+                    channel=CHANNEL_ID,
+                    thread_ts=event.get('ts'),
+                    text=MENTION_USER_WITH_MSG.format(filter_keyword),
+                )
+                print(res)
+    print('------- end ------')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+------------------------------------------------------------------------------------------------------
+
+import requests
+import os
+import slack
+import validators
+from bs4 import BeautifulSoup
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
+from slack_sdk.errors import SlackApiError
+
+
+app = Flask(__name__)
+SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
+SIGNING_SECRET = os.environ['SIGNING_SECRET']
+CHANNEL_ID = os.environ['CHANNEL_ID']
+SERVER_KEYWORD = os.environ['SERVER_KEYWORD']
+MAILBOX_KEYWORD = os.environ['MAILBOX_KEYWORD']
+MENTION_USER_WITH_MSG = os.environ['MENTION_USER_WITH_MSG']
+slack_event_adapter = SlackEventAdapter(SIGNING_SECRET, '/slack/events', app)
+client = slack.WebClient(token=SLACK_BOT_TOKEN)
+
+print('----hiii-----')
+print(slack_event_adapter)
+
+@slack_event_adapter.on('message')
+def message(payload):
+    try:
+        print('------- start ------')
+        event = payload.get('event', {})
+        if 'text' in event:
+            text_link = event.get('text')
+            text_link = text_link[1:]
+            text_link = text_link[:-1]
+            content_res = ''
+            filter_keyword = ''
+            if validators.url(text_link):
+                html = requests.get(text_link)
+                soup = BeautifulSoup(html.text, 'html.parser')
+                entry_data = soup.find_all('div', {'class': 'entry'})
+                for entry in entry_data:
+                    content_res = entry.find('div', {'class': 'entry-body'}).text
+
+                if SERVER_KEYWORD in content_res:
+                    filter_keyword = '「{0}」'.format(SERVER_KEYWORD)
+                if MAILBOX_KEYWORD in content_res:
+                    filter_keyword = '{0}「{1}」'.format(filter_keyword, MAILBOX_KEYWORD)
+
+                if filter_keyword:
+                    result = client.chat_postMessage(
+                        channel=CHANNEL_ID,
+                        thread_ts=event.get('ts'),
+                        text=MENTION_USER_WITH_MSG.format(filter_keyword),
+                    )
+                    print(result)
+        print('------- end ------')
+
+    except SlackApiError as e:
+        print(f'Error: {e}')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
